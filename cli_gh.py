edd43785cfae2b07d55a4c417aa0471c6a9cc7a7
Brian Huang <huangbrian15@gmail.com>
2020-11-16 00:18:38 -0500

    Second commit

--- cli_gh.py
+++ cli_gh.py
@@ -9,9 +9,8 @@ class GithubParser:
 
     def scrape_page(self, page_num):
         # TODO: complete this. You can access self.url_base.
-        url = "FIXME"
-        req = requests.get(url)
-        
+        url = self.url_base + "?page=" + str(page_num)
+        req = requests.get(url)    
         # This converts the json in the response to a list or dict
         list_of_jobs = req.json() 
         return list_of_jobs
@@ -19,12 +18,20 @@ class GithubParser:
     def scrape_jobs(self):
         for page_num in range(1, 20):
             jobs_in_page = self.scrape_page(page_num)
-            self.jobs += jobs_in_page # why do we call += instead of append()?
+            if jobs_in_page:
+                self.jobs += jobs_in_page
+            else:
+                break
+            # why do we call += instead of append()? 
+            # Because you cannot append a list to a another list. We are not adding 1 particular item or index to the list but rather another list
 
     def search_jobs(self, keyword, max_count):
         # TODO: filter the jobs that have the keyword in their title.
         # Try not to be case sensitive.
         filtered_jobs = []
+        for job in self.jobs:
+            if keyword.lower() in job['title'].lower():
+                filtered_jobs.append(job)
         return filtered_jobs[:max_count]
 
 class InteractiveTerminal:
@@ -41,13 +48,18 @@ class InteractiveTerminal:
 
     def ask_search(self):
         res = input("What kind of job would you like to search for? \n")
+        count = 10
+        print(self.gh.search_jobs(res, count))
         # TODO: print the names of the top ten jobs that match the search
-        pass
+
+        
 
     def request_loop(self):
         # TODO: continue running until the user indicates they want to stop.
         # Feel free to refer to the tictactoe game
-        pass
+        self.ask_search()
+        if self.ask_continue():
+            self.request_loop()
 
 if __name__ == '__main__':
     terminal = InteractiveTerminal()
