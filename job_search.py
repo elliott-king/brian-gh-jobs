667cf92dd1111cdbee10dcda27cf7e7056a9cf5e
elliott-king <8884571+elliott-king@users.noreply.github.com>
2020-11-20 11:44:26 -0500

    finalize jobsearch route

--- job_search.py
+++ job_search.py
@@ -2,12 +2,13 @@ from flask import Flask, render_template, request, redirect
 from cli_gh import GithubParser
 
 app = Flask(__name__)
+GH = GithubParser()
+GH.scrape_jobs()
 
 @app.route("/")
 def hello():
-    gh = GithubParser()
-    job_scrape = gh.scrape_jobs
-    job_init = gh.jobs
+    job_init = GH.jobs
+    print('job init', len(job_init))
     return render_template("index.html", job_init = job_init)
     
 @app.route("/jobsearch", methods=["GET","POST"])
@@ -21,4 +22,7 @@ def job_search():
         search_limit = request.form.get("search_limit")
         if not search_limit:
             search_limit = 20
-        
+        filtered_jobs = GH.search_jobs(job_title, int(search_limit))
+
+        # redirect to '/' and also with filtered_jobs
+        return render_template('index.html', job_init=filtered_jobs)
